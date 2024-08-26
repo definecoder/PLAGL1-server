842db5b8c7c9012f298b7f7258662523e3b86abb
shawon-majid <shawonmajid@gmail.com>
2024-08-26 11:09:45 +0600

    login/authentication done

---
+++ api/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+# from sqlalchemy.orm import Session
+# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+# from jose import JWTError, jwt
+# from datetime import datetime, timedelta, timezone
+# from passlib.context import CryptContext
+# from api.models.models import User
+# from database import SessionLocal, engine
+# from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+# from database import get_db
+# from core.security import hash_password, verify_password
+
+from routers.auth_router import router as auth_router
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  
+    "https://yourfrontenddomain.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows all origins from the list
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+app.include_router(auth_router)
+
+
+# Authenticate the user
+
+
+# Create access token
+
+
+
+
+
+
+
+
---
+++ api/models/schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr  
+    name: str
+    password: str
+
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+    
